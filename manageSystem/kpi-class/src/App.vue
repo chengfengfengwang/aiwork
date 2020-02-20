e6fa85400931cf8e30cdd6570c7cdedd1fe464ac
wcf <602120734@qq.com>
2020-02-20 16:11:40 +0800

    班级增加口琴

--- manageSystem/kpi-class/src/App.vue
+++ manageSystem/kpi-class/src/App.vue
@@ -65,6 +65,10 @@ export default {
         {
           label: "钢琴",
           value: "piano"
+        },
+         {
+          label: "口琴",
+          value: "harmonica"
         }
       ]
     };
