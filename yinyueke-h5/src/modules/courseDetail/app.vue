d183388d8f5f04a16e6bb0947d8e6eca93d61bcc
wcf <602120734@qq.com>
2020-04-01 11:10:13 +0800

    课程文案修改

--- yinyueke-h5/src/modules/courseDetail/app.vue
+++ yinyueke-h5/src/modules/courseDetail/app.vue
@@ -89,14 +89,14 @@
             </div>
 
             <div v-if="card.isVip" class="course">
-              <div v-show="card.isVip && card.vip_type==='vip_inf_10'" class="course_title">购会员解锁全部课程   有效期5年，限时买再赠5年</div>
+              <div v-show="card.isVip && card.vip_type==='vip_inf_10'" class="course_title">购会员解锁全部课程   有效期5年</div>
               <div v-show="card.isVip && card.vip_type==='vip_inf'" class="course_title"> 购会员解锁全部课程</div>
               <div class="course_intro">
                 <div>包含音乐素养、非洲鼓、尤克里里三大品类所有课程（含</div>
                 <div>未来上线的AI智能互动课程</div>
               </div>
               <div class="course_date">
-                <span v-show="card.isVip && card.vip_type==='vip_inf_10'">买5赠5，共10年有效期</span>
+                <span v-show="card.isVip && card.vip_type==='vip_inf_10'">5年有效期</span>
                 <span v-show="card.isVip && card.vip_type==='vip_inf'">终身有效</span>
                 <span @click="goVipDetail(index)" class="right">查看权益>></span>
               </div>
@@ -160,14 +160,14 @@
             </div>
 
             <div v-if="card.isVip" class="course">
-              <div v-show="card.isVip && card.vip_type==='vip_inf_10'" class="course_title">购会员解锁全部课程   有效期5年，限时买再赠5年</div>
+              <div v-show="card.isVip && card.vip_type==='vip_inf_10'" class="course_title">购会员解锁全部课程   有效期5年</div>
               <div v-show="card.isVip && card.vip_type==='vip_inf'" class="course_title"> 购会员解锁全部课程</div>
               <div class="course_intro">
                 <div>包含音乐素养、非洲鼓、尤克里里三大品类所有课程（含</div>
                 <div>未来上线的AI智能互动课程</div>
               </div>
               <div class="course_date">
-                <span v-show="card.isVip && card.vip_type==='vip_inf_10'">买5赠5，共10年有效期</span>
+                <span v-show="card.isVip && card.vip_type==='vip_inf_10'">5年有效期</span>
                 <span v-show="card.isVip && card.vip_type==='vip_inf'">终身有效</span>
                 <span @click="goVipDetail(index)" class="right">查看权益>></span>
               </div>
