6e18c2fbbffab5f202e120dc41b608c21f2cf188
wangchengfeng <602120734@qq.com>
2020-05-28 16:17:22 +0800

    卡林吧和钢琴svip

--- activity/src/modules/normal/router.js
+++ activity/src/modules/normal/router.js
@@ -6,6 +6,7 @@ const home = () => import('./src/index.vue')
 const musicRadio = () => import('./src/musicRadio.vue')
 const songList = () => import('./src/songList.vue')
 const play = () => import('./src/play.vue')
+const bg100 = () => import('./src/bg100.vue')
 
 Vue.use(Router)
 
@@ -30,6 +31,11 @@ export default new Router({
       path: '/play',
       name: 'play',
       component: play
+    },
+    {
+      path: '/bg100',
+      name: 'bg100',
+      component: bg100
     }
   ]
 })
---
+++ activity/src/modules/normal/src/bg100.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="wrapper">
+    <img v-show="kalimbaSvipShow" class="imgw100" src="../../../assets/img/normal/normal/kalimbaSvip.jpg" alt>
+    <img v-show="pianoSvipShow" class="imgw100" src="../../../assets/img/normal/normal/pianoSvip.jpg" alt>
+  </div>
+</template>
+<script>
+import { getQueryVariable } from "../../../common/util.js";
+
+export default {
+  data() {
+    return {
+      kalimbaSvipShow:false,
+      pianoSvipShow:false,
+    };
+  },
+  created(){
+      this.kalimbaSvipShow = getQueryVariable('show')==='kalimbaSvip';
+      this.pianoSvipShow = getQueryVariable('show')==='pianoSvip';
+  }
+};
+</script>
+<style lang="less" scoped>
+#wrapper{
+    font-size: 0
+}
+</style>
